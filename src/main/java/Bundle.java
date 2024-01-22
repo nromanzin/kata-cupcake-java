a3e3251decbf3288dcc6bbbec8c544dfd98a55da
nromanzin <nicolas.romanzin@gmail.com>
2024-01-22 11:49:51 +0100

    fix price calculation of bundles

--- src/main/java/Bundle.java
+++ src/main/java/Bundle.java
@@ -1,13 +1,19 @@
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Bundle implements Cake {
     private static final BigDecimal BUNDLE_REDUCTION = new BigDecimal("0.9");
     private final List<Cake> cakes;
+    private boolean isRoot = true;
 
     public Bundle(Cake... cakes) {
         super();
+        List<Bundle> bundles = Stream.of(cakes).filter(cake -> cake instanceof Bundle).map(cake -> (Bundle) cake).toList();
+        for (Bundle bundle : bundles) {
+            bundle.isRoot = false;
+        }
         this.cakes = List.of(cakes);
     }
 
@@ -23,7 +29,7 @@ public class Bundle implements Cake {
     public BigDecimal getPrice() {
         return cakes.stream()
                 .map(Cake::getPrice)
-                .map(price -> price.multiply(BUNDLE_REDUCTION))
+                .map(price -> price.multiply(isRoot ? BUNDLE_REDUCTION : BigDecimal.valueOf(1)))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
